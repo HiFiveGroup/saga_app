96775c600f7ebbec0223621154221a5c82a68561
mariaperhaps <mariatschettino@gmail.com>
2014-10-02 17:59:17 -0400

    models, controllers, and migrations created

---
+++ saga_app/app/controllers/sagas_controller.rb
@@ -0,0 +1,74 @@
+class SagasController < ApplicationController
+  before_action :set_saga, only: [:show, :edit, :update, :destroy]
+
+  # GET /sagas
+  # GET /sagas.json
+  def index
+    @sagas = Saga.all
+  end
+
+  # GET /sagas/1
+  # GET /sagas/1.json
+  def show
+  end
+
+  # GET /sagas/new
+  def new
+    @saga = Saga.new
+  end
+
+  # GET /sagas/1/edit
+  def edit
+  end
+
+  # POST /sagas
+  # POST /sagas.json
+  def create
+    @saga = Saga.new(saga_params)
+
+    respond_to do |format|
+      if @saga.save
+        format.html { redirect_to @saga, notice: 'Saga was successfully created.' }
+        format.json { render :show, status: :created, location: @saga }
+      else
+        format.html { render :new }
+        format.json { render json: @saga.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /sagas/1
+  # PATCH/PUT /sagas/1.json
+  def update
+    respond_to do |format|
+      if @saga.update(saga_params)
+        format.html { redirect_to @saga, notice: 'Saga was successfully updated.' }
+        format.json { render :show, status: :ok, location: @saga }
+      else
+        format.html { render :edit }
+        format.json { render json: @saga.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sagas/1
+  # DELETE /sagas/1.json
+  def destroy
+    @saga.destroy
+    respond_to do |format|
+      format.html { redirect_to sagas_url, notice: 'Saga was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_saga
+      @saga = Saga.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def saga_params
+      params.require(:saga).permit(:title, :user_id)
+    end
+end
---
+++ saga_app/db/schema.rb
@@ -0,0 +1,67 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141002214836) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "chapters", force: true do |t|
+    t.string   "title"
+    t.string   "desciption"
+    t.string   "image_url"
+    t.string   "scope"
+    t.string   "tags"
+    t.string   "category"
+    t.integer  "saga_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "chapters", ["saga_id"], name: "index_chapters_on_saga_id", using: :btree
+
+  create_table "comments", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "chapter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["chapter_id"], name: "index_comments_on_chapter_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "sagas", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sagas", ["user_id"], name: "index_sagas_on_user_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "about_me"
+    t.string   "image_url"
+    t.string   "gender"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
