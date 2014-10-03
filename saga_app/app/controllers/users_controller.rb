94ef9688c52f1d4ee143d8fcf32b88c81b186515
jennynguyen <jennynguyen622@gmail.com>
2014-10-03 12:29:53 -0400

    log in page

--- saga_app/app/controllers/users_controller.rb
+++ saga_app/app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate,            except: [:new, :create]
+  before_action :load_user,               except: [:index, :new, :create]
+  before_action :authorize_admin_only,    only: :index
+  before_action :authorize_user_only,     only: :show
+  before_action :authorize_user_or_admin, except: [:index, :show, :new, :create]
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    # @users = User.all.sort.reverse.reject {|user| user == current_user}
   end
 
   # GET /users/1
@@ -21,20 +26,13 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def password
+  end
+
   # POST /users
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /users/1
@@ -54,11 +52,18 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if current_user == @user
+      @user.destroy
+      log_out!
+      flash[:notice] = "Chao tam biet!"
+      redirect_to(root_path)
+    else #admin is deleting....
+      @user.destroy
+      redirect_to(users_path)
     end
+  end
+
+  def confirm_delete
   end
 
   private
@@ -69,6 +74,47 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :about_me, :image_url, :gender, :city, :state, :country)
+      params.require(:user).permit(
+        :name,
+        :email,
+        :about_me,
+        :image_url,
+        :gender,
+        :city,
+        :state,
+        :country,
+        :password
+        )
+    end
+
+    def user_password_params
+      @user_password_params ||= params.require(:user).permit(
+        :old_password,
+        :password,
+        :password_confirmation
+        )
+    end
+
+    def load_user
+      @user = User.find_by(id: params[:id])
+      redirect_to root_path if !@user
+    end
+
+    def authorize_admin_only
+      unless current_user.is_admin?
+        redirect_to user_path(current_user)
+      end
+    end
+
+    def authorize_user_only
+      unless current_user == @user
+        redirect_to user_path(current_user)
+      end
+    end
+
+    def authorize_user_or_admin
+      unless current_user == @user || current_user.is_admin?
+        redirect_to user_path(current_user)
+      end
     end
 end
