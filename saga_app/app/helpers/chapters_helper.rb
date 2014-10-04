84c2c87cb1b5282dd597c119574485e39fee2ee8
primitivereflex <primitivereflex@gmail.com>
2014-10-04 15:45:49 -0400

    Add add_tag method comments

--- saga_app/app/helpers/chapters_helper.rb
+++ saga_app/app/helpers/chapters_helper.rb
@@ -1,6 +1,8 @@
 module ChaptersHelper
 
   def add_tag(chapter_id, tag_string)
+    # this method can add either one tag at a time
+    # or multiple tags as a string divided by commas
     chapter = Chapter.find(chapter_id)
     chapter.tag_list.add(tag_string)
   end
