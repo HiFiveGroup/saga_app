3b530fb795ce3edbfcf97cbdca41ef9090d6ffa7
mariaperhaps <mariatschettino@gmail.com>
2014-10-04 16:03:32 -0400

    fixed logout problem

--- saga_app/app/controllers/application_controller.rb
+++ saga_app/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionHelper
-  helper_method :current_user, :logged_in?, :authenticate
+  helper_method :current_user, :logged_in?, :authenticate, :log_out!
 
 end
