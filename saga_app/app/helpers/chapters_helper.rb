9022f631c8ce38f8183b27ea4d720609f3a05e34
primitivereflex <primitivereflex@gmail.com>
2014-10-04 15:55:51 -0400

    Fix add_tag method bug; now saves tags

--- saga_app/app/helpers/chapters_helper.rb
+++ saga_app/app/helpers/chapters_helper.rb
@@ -1,10 +1,18 @@
 module ChaptersHelper
 
+  def chapters_by_category
+
+  end
+
   def add_tag(chapter_id, tag_string)
     # this method can add either one tag at a time
     # or multiple tags as a string divided by commas
     chapter = Chapter.find(chapter_id)
     chapter.tag_list.add(tag_string)
+    chapter.save
   end
 
 end
+
+# require './app/helpers/chapters_helper'
+# include (ChaptersHelper)
