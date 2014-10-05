6e154ad54184e80181aad03e40cd65e060e7e07e
primitivereflex <primitivereflex@gmail.com>
2014-10-05 16:46:29 -0400

    Change method names to find_by_tag, find_by_category

--- saga_app/app/helpers/chapters_helper.rb
+++ saga_app/app/helpers/chapters_helper.rb
@@ -1,6 +1,6 @@
 module ChaptersHelper
 
-  def self.chapters_by_category(category)
+  def self.find_by_category(category)
     # this method returns all chapters with category
     category_chapters = Chapter.where(category: category)
   end
@@ -9,19 +9,21 @@ module ChaptersHelper
     # this method adds tags to a chapter
     # either one at a time or multiple tags as a string divided by commas
     # all tags are downcased to avoid search difficulties
+    tag_string.downcase!
     chapter = Chapter.find(chapter_id)
-    chapter.tag_list.add(tag_string.downcase, parse: true)
+    chapter.tag_list.add(tag_string, parse: true)
     chapter.save
   end
 
   def remove_tag_from_chapter(chapter_id, tag_string)
     # this method removes tags from a chapter one at a time
+    tag_string.downcase!
     chapter = Chapter.find(chapter_id)
-    chapter.tag_list.remove(tag_string.downcase)
+    chapter.tag_list.remove(tag_string)
     chapter.save
   end
 
-  def self.chapters_by_tag(tag_string)
+  def self.find_by_tag(tag_string)
     # this returns a list of all chapters containing the chosen tag or tags
     Chapter.tagged_with([tag_string])
   end
@@ -32,3 +34,6 @@ module ChaptersHelper
   end
 
 end
+
+# require './app/helpers/chapters_helper'
+# include (ChaptersHelper)
