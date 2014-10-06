490fefc2a64735595d629e9a2e56e8d96e8285bc
lwschjang <leamia.schjang@gmail.com>
2014-10-06 09:22:30 -0400

    still haven't fixed footer

--- saga_app/app/controllers/chapters_controller.rb
+++ saga_app/app/controllers/chapters_controller.rb
@@ -31,31 +31,16 @@ class ChaptersController < ApplicationController
   # POST /chapters
   # POST /chapters.json
   def create
-
-    saga = Saga.find_by(title: params[:saga])
-    saga_id = saga.id
-    chapter = Chapter.create(chapter_params)
-    chapter.saga_id = saga_id
-    chapter.save
-
-
-    # @chapter = Chapter.create(title: params[:chapter][:title], user_id: session[:user_id])
-    # redirect_to(user_path(session[:user_id]))
-
-    # @chapter = Chapter.create(chapter_params)
-
-    # respond_to do |format|
-    #   if @chapter.save
-    #     format.html { redirect_to @chapter, notice: 'Chapter was successfully created.' }
-    #     format.json { render :show, status: :created, location: @chapter }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @chapter.errors, status: :unprocessable_entity }
-    #   end
-    # end
-
-     redirect_to user_path(session[:user_id])
-
+      if params[:chapter][:secret]
+       chapter = Chapter.create(chapter_params)
+      else
+      saga = Saga.find_by(title: params[:saga])
+      saga_id = saga.id
+      chapter = Chapter.create(chapter_params)
+      chapter.saga_id = saga_id
+      chapter.save
+    end
+    redirect_to user_path(session[:user_id])
   end
 
   # PATCH/PUT /chapters/1
