51262fa036296d5002fd4e65b3409ee715d076c3
rsilverblatt88 <rsilverblatt88@gmail.com>
2014-10-07 09:44:29 -0400

    splash page render fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	@chapters = Chapter.all
+  	@chapters = Chapter.where(saga_id: nil)
     if session[:user_id]
       user = User.find_by(id: session[:user_id])
       redirect_to(user_path(user))
